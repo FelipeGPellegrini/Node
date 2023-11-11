177f5438254510cad52c959c46f4a55a214146a6
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-11-11 12:51:01 -0300

    Initial commit

---
+++ inicio/app.js
@@ -0,0 +1,35 @@
+let calculadora = require("./calculadora")
+
+console.log(calculadora.soma(30,20))
+let resultado = calculadora.mult(10,20)
+let resultadoSub = calculadora.sub(20,40)
+console.log(calculadora.div(40, 20))
+console.log(resultado)
+console.log(resultadoSub)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let mostrarSite = true;
+// const site = "www.github.com"
+
+// console.log("Hello World!")
+// console.log("Meu nome é Felipe!")
+// console.log("E eu estou aprendendo Node.js")
+
+
+// if(mostrarSite) {
+//     console.log(site) 
+// }
