55ea7a11683a71d96f31383cc3a7eb11d07eab4e
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-11-27 18:38:42 -0300

    params

--- express/index.js
+++ express/index.js
@@ -1,6 +1,41 @@
 const express = require("express"); // Importando o express
 const app = express(); // Iniciando o express
 
+app.get("/", function(req, res){
+    res.send("Resposta da rota")
+});
+
+app.get("/blog/:artigo?", function(req, res) {
+
+    let artigo = req.params.artigo
+    if (artigo){
+        res.send(`<h2>Artigo ${artigo}</h2>`)
+    }else {
+        res.send("Bem vindo ao meu blog!")
+    }
+
+    
+})
+
+app.get("/canal/youtube", function(req, res) {
+    let nome = req.query["nome"]
+    if(nome){
+        res.send(nome)
+    }else {
+        res.send("Nenhum canal fornecido")
+    }
+    
+})
+
+app.get("/ola/:nome/:empresa", function(req, res) {
+    // REQ => Dados enviados pelo usuário
+    // RES => Respostas enviadas para o usuário
+    let nome = req.params.nome
+    let empresa = req.params.empresa
+    res.send(`<h1>Oi ${nome} da ${empresa}</h1>`)
+})
+
+
 app.listen(4000, function (erro) {
     if(erro) {
         console.log("Ocorreu um erro!")
