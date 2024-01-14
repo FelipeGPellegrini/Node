e505ccfaf6676ca5693474c9216ff6770ec0705b
Felipe Gomes Pellegrini <lipegomespellegrini10@gmail.com>
2024-01-14 16:45:23 -0300

    Update index.js

--- express/index.js
+++ express/index.js
@@ -39,6 +39,6 @@ app.listen(4000, function (erro) {
     if(erro) {
         console.log("Ocorreu um erro!")
     } else {
-        console.log("Servidor iniciado com sucesso")
+        console.log("Servidor iniciado")
     }
 })
